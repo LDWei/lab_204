d2e0b2e221c85e522d71c97402f07a2630f7326d
passenger_we <2602662961@qq.com>
2017-11-28 21:28:08 +0800

    时间本地化&文章详情页

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,13 @@ Route::get('/welcome', 'HomeController@welcome')->name('welcome');
 //********************************user_page************************************
 Route::get('/user/edit','UserController@showEdit')->name('user.edit_page');//为什么要放在它的上面才可以查询
 Route::patch('/user/save/{id}','UserController@saveEdit')->name('user.save_edit');
-
 Route::get('/user/{id}/edit_avatar','UserController@showAvatarEdit')->name('user.avatar_edit');
 Route::get('/user/{id}/edit_password','UserController@showPasswordEdit')->name('user.password_edit');
-
 Route::get('/user/{id}','UserController@show')->name('user.page');
 
-Route::get('/detial',function(){
-    return view('user.article_detial');
-})->name('user.page');
+//******************************articles**************************************
+Route::get('/articles/{id}','PostController@detail')->name('articles.detail');
+Route::get('/articles/{id}/edit','PostController@edit')->name('articles.edit');
 
 
 //User
