7f0894355c6d87745c1bf4e33d4c52d794f590a3
passenger_we <2602662961@qq.com>
2017-12-25 16:43:39 +0800

    点赞部分

--- resources/views/user/partials/vote.blade.php
+++ resources/views/user/partials/vote.blade.php
@@ -4,28 +4,28 @@
 
         <div class="btn-group">
 
-            <a data-ajax="post" id="vote" href="javascript:;" onclick="vote(this,{{ $posts->id }})" data-url="" title=""  id="up-vote" class="vote btn btn-primary" data-original-title="Up Vote">
+            <a data-ajax="post" id="vote" href="javascript:;" onclick="vote(this,{{ $posts->id }},{{ Auth::user()->id }})" data-url="" title=""  id="up-vote" class="vote btn btn-primary" data-original-title="Up Vote">
                 <i  class="fa fa-thumbs-up" aria-hidden="true"></i>
                 {{ Auth::user()->hasVotedFor($posts->id) ? '已赞': '点赞'}}
             </a>
         </div>
         <div class="voted-users">
             @if( $posts->vote_count == 0 )
-                <div class="vote-hint">
+                <div class="vote-hint" id="vote-hint" style="display:block">
                 成为第一个点赞的人吧 <img title=":bowtie:" alt=":bowtie:" class="emoji" src="https://dn-phphub.qbox.me/assets/images/emoji/bowtie.png" align="absmiddle">
                 </div>
-            @else
-                <div class="voted-users">
-                    <div class="user-lists">
+            @endif
+                <div class="voted-users" id="voted-users" style="display:block">
+                    <div class="user-lists" id="lists">
                         @foreach($whos as $who)
-                            <a href="{{ route('user.page',$who->id) }}" data-userid="{{ $who->name }}">
+                            <a href="{{ route('user.page',$who->id) }}" data-userid="{{ $who->id }}">
                                 <img class="img-thumbnail avatar avatar-middle" src="{{ url($who->avatar) }}" style="width:48px;height:48px;"></a>
                         @endforeach
                     </div>
                     <a class="voted-template" href="" data-userid="" style="display:none">
                         <img class="img-thumbnail avatar avatar-middle" src="" style="width:48px;height:48px;"></a>
                 </div>
-            @endif
+
             <a class="voted-template" href="" data-userid="" style="display:none">
                 <img class="img-thumbnail avatar avatar-middle" src="" style="width:48px;height:48px;"></a>
         </div>
@@ -44,17 +44,31 @@
     }
 </style>
 <script>
-    function vote(obj,posts_id){
+    function vote(obj,posts_id,user_id){
         $.get("/vote/" + posts_id,
         function (data) {
             var num=Number($('#vote-num').html());
             if(data.voted){
+                var id = data.user_id;
+                var user = '/user/'+id;
+                var avatars = '/'+data.user_avatar;
+                var cla = "img-thumbnail avatar avatar-middle";
+                var sty = "width:48px;height:48px;";
                 $(obj).html('已赞');
                 $('#vote-num').html(++num);
+                if(num>0){
+                    $('#vote-hint').css('display','none');
+                    $('#voted-users').css('display','block');
+                    $('#lists').prepend(" <a href="+user+" data-userid="+id+" data-userid="+id+" ><img src="+avatars+" class="+cla+" style="+sty+" ></a>");
+                }
             }
             else{
                 $(obj).html('点赞')
                 $('#vote-num').html(--num);
+                //alert(user_id);
+                var uid = "[data-userid="+user_id+"]";
+                //alert(uid);
+                $('#lists').find(uid).remove();
             }
         }
         );
